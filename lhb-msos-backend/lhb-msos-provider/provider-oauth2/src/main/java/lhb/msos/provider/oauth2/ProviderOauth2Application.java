02157a6937effa891ef741ee3d073acce7266d30
a782272323 <782272323@qq.com>
2020-09-27 17:03:54 +0800

    jenkins部署环境变更

--- lhb-msos-backend/lhb-msos-provider/provider-oauth2/src/main/java/lhb/msos/provider/oauth2/ProviderOauth2Application.java
+++ lhb-msos-backend/lhb-msos-provider/provider-oauth2/src/main/java/lhb/msos/provider/oauth2/ProviderOauth2Application.java
@@ -2,9 +2,10 @@ package lhb.msos.provider.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * @Description  认证服务启动类
@@ -15,9 +16,18 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan(basePackages = {"lhb.msos"})
-//@EnableResourceServer
-public class ProviderOauth2Application {
+public class ProviderOauth2Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ProviderOauth2Application.class, args);
+    }
+
+    /**
+     * 为了打包springboot项目
+     * @param builder
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 }
